22db323849703a454dd1b43b75122dc652184aae
Dany Cheong <danycheong@unitec.edu>
2015-05-16 11:13:54 -0600

    insert y indexof

--- fsarraylist.cpp
+++ fsarraylist.cpp
@@ -15,10 +15,29 @@ FSArrayList::~FSArrayList(){
 
 }
 
-bool FSArrayList::insert(Object*, int){
+bool FSArrayList::insert(Object* E, int p){
+    if(p < 0 || p >= capacity)
+        return false;
+    else {
+        if(p >=size ){
+            array[size] = E;
+            size++;
+        }else{
+            for (int i = size; i > p; i--){
+                array[i] = array[i-1];
+            }
+            array[p] = E;
+            size++;
+        }
+    }
+    return true;
 
 }
-int FSArrayList::indexOf(Object*)const{
+int FSArrayList::indexOf(Object* E)const{
+    for(int i = 0; i < size; i++)
+        if( array[i]->equals(E))
+            return i;
+    return -1;
 
 }
 Object* FSArrayList::get(unsigned int)const{
