528275b767cdc5aa5b764c509b65fdd416806c73
Dany Cheong <danycheong@unitec.edu>
2015-05-16 11:22:51 -0600

    FSArrayList final

--- fsarraylist.cpp
+++ fsarraylist.cpp
@@ -40,11 +40,26 @@ int FSArrayList::indexOf(Object* E)const{
     return -1;
 
 }
-Object* FSArrayList::get(unsigned int)const{
-
+Object* FSArrayList::get(unsigned int p)const{
+    if(size == 0)
+        return NULL;
+    if(p<0 || p >=size)
+        return NULL;
+   return array[p];
 }
-bool FSArrayList::erase(unsigned){
 
+bool FSArrayList::erase(unsigned p){
+    Object* temp;
+    if(p < 0 || p >= size )
+        return false;
+    else{
+        temp = array[p];
+        for (int i = p; i < size; ++i){
+            array[p] = array[p+1];
+        }
+        size--;
+    }
+    return true;
 }
 
 int FSArrayList::prev(int p)const{
@@ -56,16 +71,28 @@ int FSArrayList::next(int p)const{
 }
 
 void FSArrayList::reset(){
-
+    for (int i = 0; i < size; ++i){
+        delete array[i];
+    }
+    delete[] array;
+    size = 0;
 }
 Object* FSArrayList::first()const{
+    if(size == 0)
+        return NULL;    
+    return array[0];
 
 }
-Object* FSArrayList::last()const{
+Object* FSArrayList::last()const{    
+    if(size == 0)
+        return NULL;
+    return array[size-1];
 
 }
-void FSArrayList::print()const{
-
+void FSArrayList::print()const{   
+    for (int i = 0; i < size; ++i){
+        array[i]->print();
+    }
 }
 
 bool FSArrayList::isFull()const{
