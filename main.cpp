e7cc941c77f5819061f0c624be47b60c23da5f45
baquedano <baquedano@unitec.edu>
2015-05-16 13:08:54 -0600

    modificaciones iniciales al main

--- main.cpp
+++ main.cpp
@@ -1,82 +1,73 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <vector>
 #include "tdalist.h"
 #include "linkedlist.h"
+#include "fsarraylist.h"
+#include "fscursorlist.h"
 #include "integer.h"
+#include <ctime>
+#include <sstream>
+#include <stdio.h>
+#include <cstdlib>
+#include <fstream>
 
-using namespace std;
+using namespace std; 
 
-/*
-* Programa de Prueba para el TDALista implementado con LinkedList
-* -- Listas Doblemente Enlazadas --
-* Se prueban las siguientes operaciones:
-*  - Inserción
-*  - Eliminación
-*  - Listado
-*  - Búsqueda
-*/
-
-
-int menu();
-
-int main(int argc, char *argv[])
-{
-    // En el caso de que quisieramos utilizar otra implementación
-    // SÓLO se tendría que cambiar esta línea.
-    TDAList* lista = new LinkedList();
-    Integer* intTemp;
-    int val, pos;
-    bool flag = true;
-    while (flag)
-    {
-        switch(menu())
-        {
-           case 1:
-               cout << "Valor a Insertar: " ;
-               cin >> val;
-               cout << "Posicion a Insertar: ";
-               cin >> pos;
-               if (lista->insert(new Integer(val), pos))
-                  cout << "Exito en la Insercion" << endl;
-               else
-                  cout << "Fracaso en la Insercion" << endl;
-               break;
-           case 2:
-               cout << "Posicion de la que quiere Borrar: ";
-               cin >> pos;
-               if (lista->erase(pos))
-                  cout << "Exito en el Borrado" << endl;
-               else
-                  cout << "Fracaso en el Borrado" << endl;
-               break;
-           case 3:
-               lista->print();
-               break;
-           case 4:
-               cout << "Valor del Elemento a Buscar: ";
-               cin >> val;
-               intTemp = new Integer(val);
-               pos = lista->indexOf(intTemp);
-               delete intTemp;
-               cout << "El Elemento esta en la posicion = " << pos << endl;
-               break;
-           case 9:
-               flag = false;
-        }
+int main(int argc, char *argv[]){
+  ofstream file1("FSALInsert.txt");
+  //ofstream file2("FSALget.txt");
+  //ofstream file3("FSALIndex.txt");
+  //ofstream file4("FSALFirst.txt");
+  //ofstream file5("FSALLast.txt");
+  //ofstream file6("FSALRemove.txt");
+  //ofstream file7("DLinsert.txt");
+  //ofstream file8("DLremove.txt");
+  //ofstream file9("DLget.txt");
+  //ofstream file10("DLindex.txt");
+  //ofstream file11("DLfirst.txt");
+  //ofstream file12("DLlast.txt");
+  //ofstream file13("ClInsert.txt");
+  //ofstream file14("Clremove.txt");
+  //ofstream file15("Clget.txt");
+  //ofstream file16("ClIndex.txt");
+  //ofstream file17("CLfirst.txt");
+  //ofstream file18("CLlast.txt");
+  double ExecutionTime;
+  time_t start,end;
+  srand(time(0));
+  TDAList* ListaArrayList = new FSArrayList(1000);
+  TDAList* ListaLinkedList = new LinkedList();
+  TDAList* ListaCursorList = new FSCursorList(1000);
+  int posiciones[1000];
+  bool entro = false;
+  for (int z = 0; z< 1000; z++){
+    int random = rand()%1000;
+    for (int j = 0; j < 1000; j++){
+      if(random == posiciones[j]){
+        entro = true;
+        break;
+      }    
+    } 
+    if(!entro){ 
+      posiciones[z] = random; 
+    }else{      
+      z--; 
     }
-    delete lista;
+    entro = false;
+  }
 
-    return 0;
-}
-int menu()
-{
-    cout << "1. Insertar "  << endl;
-    cout << "2. Borrar "  << endl;
-    cout << "3. Listar " << endl;
-    cout << "4. Buscar " << endl;
-    cout << "9. Salir " << endl;
-    cout << endl << "Opcion? ";
-    int opcion;
-    cin >> opcion;
-    return opcion;
+  
+  for (int i = 0; i <1000; i++){ 
+    start=clock(); 
+    ListaArrayList->insert(new Integer(i),posiciones[i]);
+    end=clock();
+    ExecutionTime=(double)(end-start)/(double)CLOCKS_PER_SEC;
+    if(file1.is_open()){
+      file1<<ExecutionTime<<"\n";
+    }
+  }
+  file1.close();
+  return 0;
 }
